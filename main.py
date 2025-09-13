0d3988644fa1a1a9b63b951dca4e8c3414a6e53c
hyx-zzy <1911301363@qq.com>
2025-09-13 15:56:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-num = input("请输入一个5位数字:")
+num = input("请输入一个5位数字: ")
+
 if len(num) !=5 or not
 num.isdigit():
-  print("错误提示:请输入一个5位纯数字")
+  print("错误提示: 请输入一个5位纯数字")
 else:
   if num == num[::-1]:
     print("是回文数")
